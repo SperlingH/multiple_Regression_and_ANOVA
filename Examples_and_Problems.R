d3d5994dc1667ba03e34a66f1e8c1e7bf2df17e6
SperlingH <hts76@web.de>
2015-11-09 11:38:58 +0100

    added code and data

---
+++ Examples_and_Problems.R
@@ -0,0 +1,69 @@
+## R code for all examples and problems in "Primer of Applied Regression and Analysis of Variance" from Stanton A. Glantz & Bryan K. Slinker; 1. edition; 1990
+
+setwd("~/Dokumente/Computer/git/Primer_of_Applied_Regression_and_Analysis_of_Variance/")
+
+library("ggplot2")
+
+# Chapter 1
+# Examples
+# Table 1-1
+table.1.1 <- read.csv(url("http://people.vetmed.wsu.edu/slinkerb/appliedregression/Data%20files/Datadisk/examples/marswhw.dat"), header = F, sep="")
+table.1.1 <- data.frame(weight = table.1.1$V1,
+                        height = table.1.1$V2,
+                        water.consumption = table.1.1$V3)
+write.csv(table.1.1, file = "Table_1.1.csv",
+          row.names = F)
+
+# Fig. 1-1 plotting weight against height from Table 1-1
+ggplot(table.1.1, aes(x=height, y=weight)) +
+  geom_point(shape=1)+ 
+  geom_smooth(method=lm,   # Add linear regression line
+  se=FALSE)    # Don't add shaded confidence region
+lm(formula = weight ~ height, data = table.1.1)
+
+# Fig. 1-2 plotting weight against height from Table 1-1 with water_consumption as factor
+ggplot(table.1.1, aes(x=height, y=weight, color=as.factor(water.consumption))) +
+  geom_point(shape=1)+ 
+  geom_smooth(method=lm,   # Add linear regression line
+              se=FALSE,    # Don't add shaded confidence region
+              fullrange=TRUE) # Extend regression lines
+multiple.regression <- lm(formula = weight ~ height + water.consumption, data = table.1.1)
+multiple.regression 
+summary(multiple.regression)
+
+# Intercept <-> c1
+# height <-> m*x
+# water.consumption <-> c2*C #(Cups)
+# f(x) = c1 + m*x + c2*C
+
+####
+# Example for Dummy varialbes (D)
+table.1.2 <- read.csv(url("http://people.vetmed.wsu.edu/slinkerb/appliedregression/Data%20files/Datadisk/examples/marssmok.dat"), header = F, sep="")
+table.1.2 <- data.frame(weight = table.1.2$V1,
+                        height = table.1.2$V2,
+                        scd.hnd.smk = table.1.2$V3)
+write.csv(table.1.2, file = "Table_1.2.csv",
+          row.names = F)
+# Fig. 1-3 plotting weight against height from Table 1-1 with second hand smoke as factor
+ggplot(table.1.2, aes(x=height, y=weight, color=as.factor(scd.hnd.smk))) +
+  geom_point(shape=1)+ 
+  geom_smooth(method=lm,   # Add linear regression line
+              se=FALSE,    # Don't add shaded confidence region
+              fullrange=TRUE) # Extend regression lines
+multiple.regression <- lm(formula = weight ~ height + scd.hnd.smk, data = table.1.2)
+multiple.regression 
+summary(multiple.regression)
+
+# weight (c)
+# height (x)
+# exposure to second hand smoke: (scd.hnd.smk (C)) D = 1
+# no exposure to second hand smoke: (scd.hnd.smk(C)) D = 0
+# f(x) = c + m*x + C*D
+
+
+
+
+#####
+#input.data <- read.csv(url("http://people.vetmed.wsu.edu/slinkerb/appliedregression/Data%20files/Datadisk/problems_1ed/d-01.dat"), header = F, sep="")
+
+
