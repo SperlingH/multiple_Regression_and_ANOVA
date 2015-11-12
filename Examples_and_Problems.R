0dbec8b970c81302533e67980470c461d5080d5e
SperlingH <hts76@web.de>
2015-11-12 11:48:46 +0100

    added code for Problems Chapter 2

--- Examples_and_Problems.R
+++ Examples_and_Problems.R
@@ -80,6 +80,7 @@ summary(lm(formula = weight ~ height, data = table.2.1))
 # slope: b_1
 #   "height"
 # standard error of the estimate: s_(y|x)
+# [variability about the line of means; "average deviation of the data about the regression line"]
 #   "Residual standard error"
 # y = b_0 + b_1*x + s_(y|x)
 # used to compute confidence intervals and test hypotheses
@@ -87,10 +88,54 @@ summary(lm(formula = weight ~ height, data = table.2.1))
 # standard error of the intercept: column "Std.Error" in row "(Intercept)"
 # t-statistics in the columns "t value", corresponding P-value in "Pr(>|t|)" and significance stars
 
+# R-squared (coefficient of determination): 
+# Square of the Pearson product-moment correlation coefficient
+# R^2 describes how well the regression line represents the realaionship between the variables
+
+# Testing the Regression as a Whole:
+# F-statistic
+# in the case of a simple linear regression: 
+# is the slope of the regression line sign. different from 0?
+# in multiple regression:
+# t-test: test for each independent variable
+# F-statistic:  test for ALL independent variables
+
 # determine confidence intervals:
 confint(lm(formula = weight ~ height, data = table.2.1))
 
+## Heat exchange in gray seals
+table.C.2 <- read.csv(url("http://people.vetmed.wsu.edu/slinkerb/appliedregression/Data%20files/Datadisk/examples/seals.dat"), header = F, sep="")
+table.C.2 <- data.frame(C.b = table.C.2$V1,
+                        degree.C = table.C.2$V2)
+write.csv(table.C.2, file = "Table_C.2.csv",
+          row.names = F)
 
+ggplot(table.C.2, aes(x=degree.C, y=C.b)) +
+  geom_point(shape=1) + 
+  geom_smooth(method=lm,   # Add linear regression line
+              se=FALSE)    # Don't add shaded confidence region
+summary(lm(formula = C.b ~ degree.C, data = table.C.2))
+# linear fit should not be used if you examine the datapoints more closely
+# examine the plots of raw data!
+
+# Problems: 
+# 2.1
+# From output of summary(lm()) of table.2.1
+results.2.1 <- summary(lm(formula = weight ~ height, data = table.2.1))
+#   slope: 0.4441
+results.2.1$coefficients[[2]]
+#   standard error of the slope: 0.0643
+results.2.1$coefficients[[4]]
+#   intercept: -6.0076
+results.2.1$coefficients[[1]]
+#   standard error of the intercept: 2.3921
+results.2.1$coefficients[[3]]
+#   correlation coefficient (sqrt(R-squared)): sqrt(0.8564) = 0.9254
+sqrt(results.2.1$r.squared)
+#   standard error of the estimate (Residual standard error): 0.964 
+results.2.1$sigma
+#   F statistic for overall goodness of fit: 47.71
+results.2.1$fstatistic[[1]]
 
 #####
 #input.data <- read.csv(url("http://people.vetmed.wsu.edu/slinkerb/appliedregression/Data%20files/Datadisk/problems_1ed/d-01.dat"), header = F, sep="")
